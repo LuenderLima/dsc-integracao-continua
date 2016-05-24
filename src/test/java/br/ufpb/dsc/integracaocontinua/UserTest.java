70329a8b93435d7bd2581c7f88594a60cac20324
luenderlima <jose.luender@dce.ufpb.br>
2016-05-24 11:12:44 -0300

    Teste passando

--- src/test/java/br/ufpb/dsc/integracaocontinua/UserTest.java
+++ src/test/java/br/ufpb/dsc/integracaocontinua/UserTest.java
@@ -14,7 +14,7 @@ public class UserTest {
 		User u = new User("Luender", "1234", "jose.luender@dce.ufpb.br");
 		UserManager um = new UserManager();
 		um.addUser(u);
-		assertEquals(um.validarUsuario("Luender", "1234"), true);
+		assertEquals(um.validarUsuario("jose.luender@dce.ufpb.br", "1234"), true);
 		
 	}
 
